08dd570bc0a0fadbf29021e575605e9069d9a658
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-19 21:39:56 +0300

    Use `static` files in `index` page

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -32,7 +32,7 @@ DEBUG = os.getenv("DJANGO_DEBUG", default=False)
 
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", default="127.0.0.1").split(" ")
-
+print(ALLOWED_HOSTS)
 
 # Application definition
 
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
