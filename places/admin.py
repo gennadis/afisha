1ad4b29940b4f4d04293476a198228bb833f723f
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-19 22:53:45 +0300

    Register `Image` model in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from places.models import Place
+from places.models import Image, Place
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
     pass
