3f5046931a232b9a75d69d3cb5a9498c9fb2cc73
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-20 14:01:10 +0300

    Display `Place.images` preview in inline

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from places.models import Image, Place
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ["preview_image"]
+
+    def preview_image(self, obj):
+        max_height = 200
+        max_width = int(
+            obj.file.width / (obj.file.height / max_height)
+        )  # keep original proportions
+        return format_html(
+            f"<img src='{obj.file.url}' width='{max_width}' height='{max_height}' />"
+        )
 
 
 @admin.register(Place)
