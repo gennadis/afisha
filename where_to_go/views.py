c26e80b26e9833b9897ff73e692e9ebc68a52ddd
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-20 11:20:07 +0300

    Move `geojson` data to index view

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,37 @@
+import json
+
 from django.shortcuts import render
 
+from places.models import Place
 
-def render_index(request):
-    return render(request, "index.html")
+
+def index(request):
+    places_collection = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {"type": "Point", "coordinates": [37.62, 55.793676]},
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json",
+                },
+            },
+            {
+                "type": "Feature",
+                "geometry": {"type": "Point", "coordinates": [37.64, 55.753676]},
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json",
+                },
+            },
+        ],
+    }
+
+    return render(
+        request,
+        template_name="index.html",
+        context={"places_collection": places_collection},
+    )
