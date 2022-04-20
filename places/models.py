b2c1f2d2f716ef851e56507abf048ed888f797ec
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-20 15:01:33 +0300

    Use `tinymce` as `Place.description_long` field editor

--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
@@ -12,7 +13,7 @@ class Place(models.Model):
         blank=True,
         null=True,
     )
-    description_long = models.TextField(
+    description_long = HTMLField(
         verbose_name="Long description",
         max_length=5000,
         blank=True,
