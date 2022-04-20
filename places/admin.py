38f54f5bd105e9011575836ecd4759bd4ef581c4
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-20 14:35:02 +0300

    Make `Place.images` sortable

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,11 @@
+from adminsortable2.admin import SortableAdminMixin, SortableInlineAdminMixin
 from django.contrib import admin
 from django.utils.html import format_html
 
 from places.models import Image, Place
 
 
-class ImageInline(admin.TabularInline):
+class SortableImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     readonly_fields = ["preview_image"]
 
@@ -19,8 +20,8 @@ class ImageInline(admin.TabularInline):
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
-    inlines = [ImageInline]
+class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
+    inlines = [SortableImageInline]
 
 
 @admin.register(Image)
