8c2cabb261684a8ca3ec4ebdea554d44a14d7fcb
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-21 20:19:32 +0300

    Fix `Place` admin field exception bugs

--- src/places/admin.py
+++ src/places/admin.py
@@ -1,4 +1,4 @@
-from adminsortable2.admin import SortableAdminMixin, SortableInlineAdminMixin
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 from django.contrib import admin
 from django.utils.html import format_html
 
@@ -9,20 +9,19 @@ class SortableImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
     readonly_fields = ["preview_image"]
     extra = 0
+    fields = ("file", "preview_image", "order")
 
     def preview_image(self, obj):
-        max_height = 200
-        max_width = int(
-            obj.file.width / (obj.file.height / max_height)
-        )  # keep original proportions
         return format_html(
-            f"<img src='{obj.file.url}' width='{max_width}' height='{max_height}' />"
+            f"<img src='{obj.file.url}' style='max-height:200px;max-width:400px' />"
         )
 
 
 @admin.register(Place)
-class PlaceAdmin(SortableAdminMixin, admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = [SortableImageInline]
+    ordering = ["title"]
+    search_fields = ["title"]
 
 
 @admin.register(Image)
