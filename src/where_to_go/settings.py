15dc367dc24cbbbf6d6549d05d082aa9b891a9eb
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-21 16:42:35 +0300

    Add media files composing

--- src/where_to_go/settings.py
+++ src/where_to_go/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 
 # Default primary key field type
