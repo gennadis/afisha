f3a8e96f6ba3936b2c59074333464a2b8e89a5f9
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-21 21:54:47 +0300

    Set django `SECRET_KEY` default value

--- src/where_to_go/settings.py
+++ src/where_to_go/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", default="SECRET_KEY_DEFAULT_VALUE")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv("DJANGO_DEBUG", default=0))
