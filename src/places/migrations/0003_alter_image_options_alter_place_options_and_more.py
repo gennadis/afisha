4cf225572351a77ccd56e8fc42581b500066d96b
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-21 20:09:38 +0300

    Remove `Place` ordering

--- src/places/migrations/0003_alter_image_options_alter_place_options_and_more.py
+++ src/places/migrations/0003_alter_image_options_alter_place_options_and_more.py
@@ -7,26 +7,28 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('places', '0002_image'),
+        ("places", "0002_image"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='image',
-            options={'ordering': ['order', 'place']},
-        ),
-        migrations.AlterModelOptions(
-            name='place',
-            options={'ordering': ['title']},
+            name="image",
+            options={"ordering": ["order", "place"]},
         ),
         migrations.AlterField(
-            model_name='image',
-            name='order',
-            field=models.PositiveSmallIntegerField(db_index=True, default=0, verbose_name='Image order'),
+            model_name="image",
+            name="order",
+            field=models.PositiveSmallIntegerField(
+                db_index=True, default=0, verbose_name="Image order"
+            ),
         ),
         migrations.AlterField(
-            model_name='image',
-            name='place',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='images', to='places.place'),
+            model_name="image",
+            name="place",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="images",
+                to="places.place",
+            ),
         ),
     ]
