8c4e18c0622b77fb42c7875ce2eb9f2acc2ae1e1
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-22 12:54:11 +0300

    Run `postgres` healthcheck to apply db migrations

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# Wait for PostgreSQL start up to 
+# collect static and run magrations
+echo "Waiting for PostgreSQL..."
+while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+done
+echo "PostgreSQL started"
+
+# Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Apply database migrations
 echo "Applying database migrations..."
 python manage.py migrate
 
